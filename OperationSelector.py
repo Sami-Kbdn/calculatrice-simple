e851d038c2e7b22ae8339107a9437412ace2b83c
NicolasCassonnet <nicolas.cassonnet@wanadoo.fr>
2024-10-23 15:33:31 +0200

    modif presentation 2

--- OperationSelector.py
+++ OperationSelector.py
@@ -1,4 +1,6 @@
 
+import math
+
 from soustraction import soustraction
 from division import division
 
@@ -7,52 +9,62 @@ import multiplication as mp
 
 
 def mainFunction() :
-    entree = input("Entrez une opération: ").replace(" ", "")
-    if "+" in entree :
-        operation = '+'
-        operandes = entree.split('+')
-    elif "-" in entree :
-        operation = '-'
-        operandes = entree.split('-')
-    elif "/" in entree :
-        operation = '/'
-        operandes = entree.split('/')
-    elif "*" in entree :
-        operation = '*'
-        operandes = entree.split('*')
-    else :
-        print("Operation non authorisée.")
-        return
-    
-    #print(operandes)
-
-    if len(operandes) !=2:
-        print("Operation inconnue.")
-        return
-
-    left = float(operandes[0])
-    right = float(operandes[1])
-    print(left)
-
-    resultat = 0
-
-    if operation == '+' :
-        resultat = ad.addition(left,right)
-        print(f"le résultat est de : {resultat}")
-    
-    elif operation == '-' :
-        resultat = soustraction(left, right)
-        print(f"le résultat est de : {resultat}")
-
-    elif operation =='*' :
-        resultat = mp.multiplication(left,right)
-        print(f"le résultat est de : {resultat}")
-
-    elif operation =='/' :
-        resultat = division(left, right)
-        print(f"le résultat est de : {resultat}")
+    entree = ""
+    print("operation> ",end='')
+    while entree !='q' :
+        entree = input().replace(" ", "")
+        if entree =="q":
+            return
         
-    else :
-        print("Operation inconnue.")
+        if "+" in entree :
+            operation = '+'
+            operandes = entree.split('+')
+        elif "-" in entree :
+            operation = '-'
+            operandes = entree.split('-')
+        elif "/" in entree :
+            operation = '/'
+            operandes = entree.split('/')
+        elif "*" in entree :
+            operation = '*'
+            operandes = entree.split('*')
+        else :
+            print("Operation non authorisée.")
+            return
+        
+        #print(operandes)
+
+        if len(operandes) !=2:
+            print("Operation inconnue.")
+            return
+
+        left = float(operandes[0])
+        right = float(operandes[1])
+
+        resultat = math.inf
+
+        if operation == '+' :
+            resultat = ad.addition(left,right)        
+        
+        elif operation == '-' :
+            resultat = soustraction(left, right)
+
+        elif operation =='*' :
+            resultat = mp.multiplication(left,right)
+
+        elif operation =='/' :
+            resultat = division(left, right)
+        else :
+            print("Operation inconnue.")
+
+        if resultat != math.inf :
+            print(f"le résultat est de : {resultat}")
+
+        print()   
+        print("operation> ",end='')
+    
+    print()   
+    print("Au revoir")
+
 
     
